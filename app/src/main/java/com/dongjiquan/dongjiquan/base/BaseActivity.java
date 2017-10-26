7e69daebf40db7bd510a7b72948a8dd8db7a82c5
wulixia <412219454@qq.com>
2017-10-26 17:12:12 +0800

    wlx

--- app/src/main/java/com/dongjiquan/dongjiquan/base/BaseActivity.java
+++ app/src/main/java/com/dongjiquan/dongjiquan/base/BaseActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.WindowManager;
+
+import com.gyf.barlibrary.ImmersionBar;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -20,19 +21,33 @@ import butterknife.ButterKnife;
 public abstract class BaseActivity<V,T extends BasePresenter<V>> extends AppCompatActivity {
 
     public T mPresenter;
+    private ImmersionBar mImmersionBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.FLAG_LOCAL_FOCUS_MODE);
+//        getWindow().setSoftInputMode(WindowManager.LayoutParams.FLAG_LOCAL_FOCUS_MODE);
         if (getLayoutId()!=0)setContentView(getLayoutId());
         ButterKnife.bind(this);
+        initImm();
         mPresenter=initPresenter();
         EventBus.getDefault().register(this);
         if (mPresenter != null) mPresenter.attach((V) this);
         initView();
+
         if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
         }
+    }
+
+    private void initImm() {
+        mImmersionBar = ImmersionBar.with(this);
+        mImmersionBar.statusBarDarkFont(true);
+//        mImmersionBar.flymeOSStatusBarFontColor(R.color.red_btn_bg_pressed_color);
+//        mImmersionBar.statusBarColorTransform(R.color.orange);
+        mImmersionBar.fitsSystemWindows(true) ;
+//        mImmersionBar .statusBarColor(R.color.red);
+        mImmersionBar.init();   //所有子类都将继承这些相同的属性
     }
 
     @Override
@@ -58,6 +73,8 @@ public abstract class BaseActivity<V,T extends BasePresenter<V>> extends AppComp
         super.onDestroy();
         if (mPresenter!=null)mPresenter.detach();
         EventBus.getDefault().unregister(this);
+        if (mImmersionBar != null)
+            mImmersionBar.destroy();  //必须调用该方法，防止内存泄漏，不调用该方法，如果界面bar发生改变，在不关闭app的情况下，退出此界面再进入将记忆最后一次bar改变的状态
     }
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void mainThread(String s){}
--- app/src/main/java/com/dongjiquan/dongjiquan/ui/main/MainActivity.java
+++ app/src/main/java/com/dongjiquan/dongjiquan/ui/main/MainActivity.java
@@ -1,17 +1,14 @@
 package com.dongjiquan.dongjiquan.ui.main;
 
-import android.graphics.Color;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.dongjiquan.dongjiquan.R;
+import com.dongjiquan.dongjiquan.base.BaseActivity;
+import com.dongjiquan.dongjiquan.base.BasePresenter;
 import com.dongjiquan.dongjiquan.ui.home.HomeFragment;
 import com.dongjiquan.dongjiquan.utils.ScaleView;
 
@@ -20,15 +17,12 @@ import java.util.List;
 import java.util.Timer;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
-
-import static com.dongjiquan.dongjiquan.R.id.tv_num;
 
 
 /*隐藏状态栏和ActionBar的方式在4.1系统之上和4.1系统之下还是不一样的，
         这里我就不准备考虑4.1系统之下的兼容性了，因为过于老的系统根本就没有提供沉浸式体验的支持。*/
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     Timer timer = new Timer();
     @BindView(R.id.main_view_pager)
@@ -36,29 +30,12 @@ public class MainActivity extends AppCompatActivity {
     List<Fragment> list = new ArrayList<>();
     @BindView(R.id.scale_view)
     ScaleView scaleView;
-    @BindView(tv_num)
+    @BindView(R.id.tv_num)
     TextView tvNum;
 
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= 21) {
-
-            //这里先调用getWindow().getDecorView()方法获取到了当前界面的DecorView
-            View decorView = getWindow().getDecorView();
-            //SYSTEM_UI_FLAG_FULLSCREEN表示全屏的意思，也就是会将状态栏隐藏。
-            int uiFlagFullscreen = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            //然后调用它的setSystemUiVisibility()方法来设置系统UI元素的可见性
-            decorView.setSystemUiVisibility(uiFlagFullscreen);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        initView();
-    }
-
-    private void initView() {
-
+    protected void initView() {
         list.add(new HomeFragment());
         list.add(new HomeFragment());
         ViewPager viewPager = (ViewPager) findViewById(R.id.main_view_pager);
@@ -92,6 +69,17 @@ public class MainActivity extends AppCompatActivity {
                 tvNum.setText(var1+"");
             }
         });
+    }
+
+
+    @Override
+    protected BasePresenter initPresenter() {
+        return null;
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
     }
 
     @OnClick()
